ace829ae13f202679946ea618aa17d04148a7314
Andrei Gurko <a_ndroon.89@mail.ru>
2021-03-20 01:15:58 +0300

    first steps for xml builder

--- xml_string_builder.py
+++ xml_string_builder.py
@@ -1,3 +1,18 @@
+from xml_string_parser import *
+from collections import deque
+
+
 class XmlStringBuilder:
     def build_tags(self, tags_data_list):
-        pass
+        tag_stack = deque()
+        root_upper_strings = []
+        for tag_data in tags_data_list:
+            if tag_data.startwith(ROOT_UPPER_TAG_BEGIN) and tag_data.endwith(ROOT_UPPER_TAG_END):
+                root_upper_strings.append(tag_data)
+                continue
+            for index, symbol in enumerate(tag_data, 0):
+                pass
+
+t = '<? bbb ?>'
+if t.startswith('<?') and t.endswith('?>'):
+    print('tag')
