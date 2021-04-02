497da41b362439686b5af42d02ded2dc72fa5651
Andrei Gurko <a_ndroon.89@mail.ru>
2021-04-03 00:00:30 +0300

    add stack class

--- xml_string_builder.py
+++ xml_string_builder.py
@@ -13,6 +13,26 @@ class XmlStringBuilder:
             for index, symbol in enumerate(tag_data, 0):
                 pass
 
-t = '<? bbb ?>'
-if t.startswith('<?') and t.endswith('?>'):
-    print('tag')
+
+class Stack:
+    def __init__(self):
+        self._storage = []
+
+    def push(self, item):
+        self._storage.append(item)
+
+    def get_peak(self):
+        item = None
+        try:
+            item = self._storage[-1]
+        except IndexError:
+            pass
+        return None
+
+    def pop(self):
+        item = None
+        try:
+            item = self._storage.pop(-1)
+        except IndexError:
+            pass
+        return item
